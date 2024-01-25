a533d92d265cac8498cc96376ca31f177cfbb67a
Abhisheklikho <111559519+Abhisheklikho@users.noreply.github.com>
2024-01-26 05:28:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+let cryptoLive = document.getElementById("cryptoLive");
+
+let capAmount ="$811.7B"
+let tagAmount ="0.17%"
+cryptoLive.innerHTML = `The global cryptocurrency market cap today is <span style ="color: #00FF00">${capAmount} </span> a <span style ="color: red">${tagAmount}</span> decrease in the last 24 hours.`;
